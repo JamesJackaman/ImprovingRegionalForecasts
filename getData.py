2966ef96e7eea82d5a59f92d8c5dd8f63a92ecaa
James Jackaman <james.jackaman@ntnu.no>
2025-02-05 13:51:21 +0100

    Improve error message if data fails to download

--- getData.py
+++ getData.py
@@ -5,4 +5,8 @@ import os
 if os.path.isfile('data_swe.pickle'):
     pass
 else:
-    os.system('wget https://folk.ntnu.no/jamesij/1d/data_swe.pickle')
+    try:
+        os.system('wget https://folk.ntnu.no/jamesij/1d/data_swe.pickle')
+    except:
+        raise ImportError('wget is used to download the dataset. ' +
+                          'Please download dataset from url manually (see getData.py)')
